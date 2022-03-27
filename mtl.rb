9f54581df1824a7c406ef46c435c29be6b79cbe8
Takahiro Masui <takahiro.masui@luxiar.com>
2022-03-27 15:17:26 +0900

    rubocop 指摘修正

--- mtl.rb
+++ mtl.rb
@@ -1,8 +1,8 @@
 #!/usr/bin/env ruby
 
 require 'thor'
-require "pry"
 
+# Mtl(MyTaskList)
 class Mtl < Thor
   MTL_TASK_LIST_FILE_PATH = ENV['MTL_TASK_LIST_FILE_PATH']
   default_command :list
@@ -10,7 +10,7 @@ class Mtl < Thor
   desc 'add', 'add task'
   def add(task)
     File.open(MTL_TASK_LIST_FILE_PATH, 'a+') do |task_list|
-      current_time = Time.now.strftime("%Y-%m-%d %H:%M")
+      current_time = Time.now.strftime('%Y-%m-%d %H:%M')
       task_list.puts("[#{current_time}] #{task}")
       puts 'succeeded!'
       task_list.seek(0)
@@ -22,16 +22,17 @@ class Mtl < Thor
   desc 'rm', 'remove task'
   def rm(index)
     new_task_list = File.read(MTL_TASK_LIST_FILE_PATH)
-      .split(/\n/)
-      .reject.with_index { |_task, i|  i == index.to_i }
-      .join("\n")
+                        .split(/\n/)
+                        .reject.with_index { |_task, i| i == index.to_i }
+                        .join("\n")
 
-    File.open(MTL_TASK_LIST_FILE_PATH, 'w') { |task_list| task_list.puts(new_task_list) }
+    File.open(MTL_TASK_LIST_FILE_PATH, 'w') do |task_list|
+      task_list.puts(new_task_list)
+    end
     puts new_task_list
   end
 
   desc 'list', 'view task list'
-  method_option :value, :default => "some value"
   def list
     File.foreach(MTL_TASK_LIST_FILE_PATH).with_index do |task, i|
       puts "#{i}: #{task}"
